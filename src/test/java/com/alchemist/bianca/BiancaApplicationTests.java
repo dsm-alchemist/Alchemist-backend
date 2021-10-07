ff89711cfac01766887c641044a09d2d7da7819b
parksangwoo <b01056721617@gmail.com>
2021-10-07 17:08:46 +0900

    📑 :: project init

---
+++ src/test/java/com/alchemist/bianca/BiancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alchemist.bianca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
