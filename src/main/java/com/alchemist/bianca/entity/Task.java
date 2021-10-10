b6ee0710e21b52adff09fe7fe177a0509c0e8127
parksangwoo <b01056721617@gmail.com>
2021-10-10 18:32:34 +0900

    [ADD] Task Entity

--- src/main/java/com/alchemist/bianca/entity/Task.java
+++ src/main/java/com/alchemist/bianca/entity/Task.java
@@ -1,2 +1,27 @@
-package com.alchemist.bianca.entity;public class Task {
+package com.alchemist.bianca.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 45)
+    private String task;
+
+    private LocalDateTime date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private User userId;
+
+    private boolean isDone;
 }
