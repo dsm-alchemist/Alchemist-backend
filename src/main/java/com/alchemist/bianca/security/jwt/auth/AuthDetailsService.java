431a0f60190ba6992fe0135c67fbfdb707a21d05
parksangwoo <b01056721617@gmail.com>
2021-10-12 20:50:55 +0900

    [ADD] AuthDetailsService

--- src/main/java/com/alchemist/bianca/security/jwt/auth/AuthDetailsService.java
+++ src/main/java/com/alchemist/bianca/security/jwt/auth/AuthDetailsService.java
@@ -1,13 +1,22 @@
-package com.alchemist.bianca.entity.security.jwt.auth;
+package com.alchemist.bianca.security.jwt.auth;
 
+import com.alchemist.bianca.entity.user.UserRepository;
+import com.alchemist.bianca.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthDetailsService implements UserDetailsService {
+
     private final UserRepository userRepository;
 
-    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+    }
 }
