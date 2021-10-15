f8e203977862dd3047adb121b7a16823f5a9a99f
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:40:36 +0900

    [ADD] RefreshTokenRepository

--- src/main/java/com/alchemist/bianca/entity/refresh_token/RefreshTokenRepository.java
+++ src/main/java/com/alchemist/bianca/entity/refresh_token/RefreshTokenRepository.java
@@ -1,2 +1,13 @@
-package com.alchemist.bianca.entity.refresh_token;public class RefreshTokenRepository {
+package com.alchemist.bianca.entity.refresh_token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
 }
+
