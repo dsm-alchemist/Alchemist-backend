0d1465086aef51b12d2a1eef8cb65262fe16a12e
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:20:21 +0900

    [ADD] ExpiredRefreshTokenException

--- src/main/java/com/alchemist/bianca/exception/ExpiredRefreshTokenException.java
+++ src/main/java/com/alchemist/bianca/exception/ExpiredRefreshTokenException.java
@@ -1,2 +1,10 @@
-package com.alchemist.bianca.exception;public class ExpiredRefreshTokenException {
+package com.alchemist.bianca.exception;
+
+import com.alchemist.bianca.error.exception.ErrorCode;
+import com.alchemist.bianca.error.exception.ServerException;
+
+public class ExpiredRefreshTokenException extends ServerException {
+    public ExpiredRefreshTokenException() {
+        super(ErrorCode.EXPIRED_REFRESH_TOKEN);
+    }
 }
