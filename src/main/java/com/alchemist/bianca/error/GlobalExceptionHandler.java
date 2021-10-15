f31dedf40975947ddec6d2d8e12e74c3323d4089
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:21:30 +0900

    [ADD] GlobalExceptionHandler

--- src/main/java/com/alchemist/bianca/error/GlobalExceptionHandler.java
+++ src/main/java/com/alchemist/bianca/error/GlobalExceptionHandler.java
@@ -1,2 +1,27 @@
-package com.alchemist.bianca.error;public class GlobalExceptionHandler {
+package com.alchemist.bianca.error;
+
+import com.alchemist.bianca.error.exception.ErrorCode;
+import com.alchemist.bianca.error.exception.ServerException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ServerException.class)
+    public ResponseEntity<ErrorResponse> serverExceptionHandler(ServerException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return new ResponseEntity<>(new ErrorResponse(errorCode.getStatus(), errorCode.getMessage()),
+                HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(new ErrorResponse(400,
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
 }
