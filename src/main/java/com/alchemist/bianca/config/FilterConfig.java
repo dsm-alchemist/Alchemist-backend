66bcb83fc10dfe31522eb7331579a1e69c7372a7
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:13:41 +0900

    [ADD] FilterConfig

--- src/main/java/com/alchemist/bianca/config/FilterConfig.java
+++ src/main/java/com/alchemist/bianca/config/FilterConfig.java
@@ -1,2 +1,24 @@
-package com.alchemist.bianca.config;public class FilterConfig {
+package com.alchemist.bianca.config;
+
+import com.alchemist.bianca.error.ExceptionHandlerFilter;
+import com.alchemist.bianca.security.jwt.JwtTokenFilter;
+import com.alchemist.bianca.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        JwtTokenFilter filter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
+    }
 }
