55064e986a1fd662e9ad3e7cbc0dc6b4658319a1
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:42:21 +0900

    [ADD] RedisConfig

--- src/main/java/com/alchemist/bianca/config/RedisConfig.java
+++ src/main/java/com/alchemist/bianca/config/RedisConfig.java
@@ -1,2 +1,39 @@
-package com.alchemist.bianca.config;public class RedisConfig {
+package com.alchemist.bianca.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.time.Duration;
+
+@Configuration
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);  // 단일 인스턴스로 redis 구현
+        if(redisPassword != null && !redisPassword.isBlank())
+            redisConfig.setPassword(redisPassword);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(1))
+                .shutdownTimeout(Duration.ZERO)
+                .build();
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
 }
