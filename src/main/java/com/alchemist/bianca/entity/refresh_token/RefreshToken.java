305c3f15039abf272e946e26d26763fa3b10dc2d
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:40:07 +0900

    [UPDATE] Id 어노테이션 import 변경

--- src/main/java/com/alchemist/bianca/entity/refresh_token/RefreshToken.java
+++ src/main/java/com/alchemist/bianca/entity/refresh_token/RefreshToken.java
@@ -1,2 +1,23 @@
-package com.alchemist.bianca.entity.refresh_token;public class RefreshToken {
+package com.alchemist.bianca.entity.refresh_token;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@AllArgsConstructor
+@RedisHash
+public class RefreshToken {
+
+    @Id
+    private final String email;
+
+    @Indexed
+    private String token;
+
+    @TimeToLive
+    private Long ttl;
 }
