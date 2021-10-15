7a0a05dfe25fc9cc9f3ea2493c539702196a5dee
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:38:08 +0900

    [ADD] Storage Entity

--- src/main/java/com/alchemist/bianca/entity/storage/Storage.java
+++ src/main/java/com/alchemist/bianca/entity/storage/Storage.java
@@ -1,2 +1,23 @@
-package com.alchemist.bianca.entity.storage;public class Storage {
+package com.alchemist.bianca.entity.storage;
+
+import com.alchemist.bianca.entity.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Storage {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private User userId;
+
+    @Column(length = 45)
+    private String task;
 }
