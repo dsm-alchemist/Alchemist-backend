c1e0d7eb9ee4cf5d0c1f0724336483f095d5b8e4
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:42:12 +0900

    [ADD] SecurityConfig

--- src/main/java/com/alchemist/bianca/config/SecurityConfig.java
+++ src/main/java/com/alchemist/bianca/config/SecurityConfig.java
@@ -1,2 +1,32 @@
-package com.alchemist.bianca.config;public class SecurityConfig {
+package com.alchemist.bianca.config;
+
+import com.alchemist.bianca.error.ExceptionHandlerFilter;
+import com.alchemist.bianca.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .sessionManagement()
+                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS); //jwt 사용 시 stateless로 설정
+        /*http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "");*/
+    }
 }
