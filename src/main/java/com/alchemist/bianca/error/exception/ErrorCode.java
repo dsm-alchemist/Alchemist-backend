e077af02b3d5285f987819d7be0f28576898a5fd
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:21:03 +0900

    [ADD] ErrorCode

--- src/main/java/com/alchemist/bianca/error/exception/ErrorCode.java
+++ src/main/java/com/alchemist/bianca/error/exception/ErrorCode.java
@@ -1,2 +1,19 @@
-package com.alchemist.bianca.error.exception;public class ErrorCode {
+package com.alchemist.bianca.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    USER_NOT_FOUND(404, "user not found"),
+    INVALID_TOKEN(401, "Invalid token"),
+    EXPIRED_ACCESS_TOKEN(401, "Expired access token"),
+    EXPIRED_REFRESH_TOKEN(401, "Expired refresh token");
+
+    private final int status;
+    private final String message;
 }
