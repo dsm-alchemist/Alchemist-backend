d10ca646ed2e9f94c3c63efda5d55db845d37386
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:22:42 +0900

    [ADD] User Entity

--- src/main/java/com/alchemist/bianca/entity/user/User.java
+++ src/main/java/com/alchemist/bianca/entity/user/User.java
@@ -1,2 +1,97 @@
-package com.alchemist.bianca.entity.user;public class User {
+package com.alchemist.bianca.entity.user;
+
+import com.alchemist.bianca.entity.follow.Follow;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+public class User implements Serializable, UserDetails {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long user_id;
+
+    @Column(length = 50, nullable = false)
+    private String email;
+
+    @Column(length = 30, nullable = false)
+    private String password;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    private LocalTime timer;
+
+    @OneToMany(mappedBy = "following", cascade = { CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Follow> following = new ArrayList<>();
+
+    @OneToMany(mappedBy = "follower", cascade = { CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Follow> follower = new ArrayList<>();
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
 }
