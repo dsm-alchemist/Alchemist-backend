763fa5386ff5510bd2f22a14e19deb9faa961fcc
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:22:17 +0900

    [ADD] ErrorResponse

--- src/main/java/com/alchemist/bianca/error/ErrorResponse.java
+++ src/main/java/com/alchemist/bianca/error/ErrorResponse.java
@@ -1,2 +1,19 @@
-package com.alchemist.bianca.error;public class ErrorResponse {
+package com.alchemist.bianca.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+
+    @Override
+    public String toString() {
+        return "ErrorResponse{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
