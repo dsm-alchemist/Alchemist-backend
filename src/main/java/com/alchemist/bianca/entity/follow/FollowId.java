fb7b6be02aa9d44ffdf25fe79a65fd19f7613eec
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:41:34 +0900

    [ADD] User, Follow 복합키 매핑

--- src/main/java/com/alchemist/bianca/entity/follow/FollowId.java
+++ src/main/java/com/alchemist/bianca/entity/follow/FollowId.java
@@ -1,2 +1,24 @@
-package com.alchemist.bianca.entity.follow;public class FollowId {
+package com.alchemist.bianca.entity.follow;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class FollowId implements Serializable {
+    @Column(name = "following")
+    private Long following;
+
+    @Column(name = "follower")
+    private Long follower;
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
