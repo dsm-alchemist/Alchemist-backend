74b031776381eafccc9329c4db3e9976c6592835
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:08:49 +0900

    [ADD] InvalidTokenException

--- src/main/java/com/alchemist/bianca/exception/InvalidTokenException.java
+++ src/main/java/com/alchemist/bianca/exception/InvalidTokenException.java
@@ -1,2 +1,11 @@
-package com.alchemist.bianca.exception;public class InvalidTokenException {
+package com.alchemist.bianca.exception;
+
+import com.alchemist.bianca.error.exception.ErrorCode;
+import com.alchemist.bianca.error.exception.ServerException;
+
+public class InvalidTokenException extends ServerException {
+
+    public InvalidTokenException() {
+        super(ErrorCode.INVALID_TOKEN);
+    }
 }
