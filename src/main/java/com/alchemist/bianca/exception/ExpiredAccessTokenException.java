40ca414c6dbc256d3b56620bb0f9398f94fc61cc
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:19:57 +0900

    [ADD] ExpiredAccessTokenException

--- src/main/java/com/alchemist/bianca/exception/ExpiredAccessTokenException.java
+++ src/main/java/com/alchemist/bianca/exception/ExpiredAccessTokenException.java
@@ -1,2 +1,11 @@
-package com.alchemist.bianca.exception;public class ExpiredAccessTokenException {
+package com.alchemist.bianca.exception;
+
+import com.alchemist.bianca.error.exception.ErrorCode;
+import com.alchemist.bianca.error.exception.ServerException;
+
+public class ExpiredAccessTokenException extends ServerException {
+
+    public ExpiredAccessTokenException() {
+        super(ErrorCode.EXPIRED_ACCESS_TOKEN);
+    }
 }
