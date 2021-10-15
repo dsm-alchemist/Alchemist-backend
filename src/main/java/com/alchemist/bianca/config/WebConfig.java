9c0a234a123eafd15e9a4ecc6c620fec9f056c34
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:41:49 +0900

    [ADD] WebConfig

--- src/main/java/com/alchemist/bianca/config/WebConfig.java
+++ src/main/java/com/alchemist/bianca/config/WebConfig.java
@@ -1,2 +1,15 @@
-package com.alchemist.bianca.config;public class WebConfig {
+package com.alchemist.bianca.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
 }
