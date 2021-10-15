d6cb7edf7e359b58e7d71eada2f7e3827c802597
parksangwoo <b01056721617@gmail.com>
2021-10-15 10:08:15 +0900

    [ADD] UserNotFoundException

--- src/main/java/com/alchemist/bianca/exception/UserNotFoundException.java
+++ src/main/java/com/alchemist/bianca/exception/UserNotFoundException.java
@@ -1,2 +1,11 @@
-package com.alchemist.bianca.exception;public class UserNotFoundException {
+package com.alchemist.bianca.exception;
+
+import com.alchemist.bianca.error.exception.ErrorCode;
+import com.alchemist.bianca.error.exception.ServerException;
+
+public class UserNotFoundException extends ServerException {
+
+    public UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
 }
