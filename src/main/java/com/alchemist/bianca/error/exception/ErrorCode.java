fda167a42576a94d459b8da80fca62510377fb3e
parksangwoo <b01056721617@gmail.com>
2021-10-17 17:22:59 +0900

    [UPDATE] Mismatched password Exception

--- src/main/java/com/alchemist/bianca/error/exception/ErrorCode.java
+++ src/main/java/com/alchemist/bianca/error/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "user not found"),
     INVALID_TOKEN(401, "Invalid token"),
     EXPIRED_ACCESS_TOKEN(401, "Expired access token"),
-    EXPIRED_REFRESH_TOKEN(401, "Expired refresh token");
+    EXPIRED_REFRESH_TOKEN(401, "Expired refresh token"),
+    MISMATCHED_PASSWORD(400, "Mismatched password");
 
     private final int status;
     private final String message;
