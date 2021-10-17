44976b1c8bdd297cbc461e3b1799ca5f04332c1c
parksangwoo <b01056721617@gmail.com>
2021-10-17 17:25:14 +0900

    [UPDATE] @NotNull 조건 추가

--- src/main/java/com/alchemist/bianca/dto/user/request/LoginRequest.java
+++ src/main/java/com/alchemist/bianca/dto/user/request/LoginRequest.java
@@ -1,2 +1,15 @@
-package com.alchemist.bianca.dto.user.request;public class LoginRequest {
+package com.alchemist.bianca.dto.user.request;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
 }
