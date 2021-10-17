765c6b09d7a5b3d97d36a3de391d2084f2d03abd
parksangwoo <b01056721617@gmail.com>
2021-10-17 17:26:24 +0900

    [ADD] Follow Repository

---
+++ src/main/java/com/alchemist/bianca/entity/follow/FollowRepository.java
@@ -0,0 +1,6 @@
+package com.alchemist.bianca.entity.follow;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, String> {
+}
