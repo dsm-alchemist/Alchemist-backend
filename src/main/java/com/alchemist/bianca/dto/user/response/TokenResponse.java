1bde60ab57724b0ddf8934759fe1ff3648d3460a
parksangwoo <b01056721617@gmail.com>
2021-10-17 17:27:46 +0900

    [ADD] TokenResponse Dto

--- src/main/java/com/alchemist/bianca/dto/user/response/TokenResponse.java
+++ src/main/java/com/alchemist/bianca/dto/user/response/TokenResponse.java
@@ -1,2 +1,12 @@
-package com.alchemist.bianca.dto.user.response;public class TokenResponse {
+package com.alchemist.bianca.dto.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private final String accessToken;
+    private final String refreshToken;
+    private final String email;
 }
