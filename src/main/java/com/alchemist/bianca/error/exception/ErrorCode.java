53b9a565b42ace9ce961f43e6581a5008ec04c6b
parksangwoo <b01056721617@gmail.com>
2021-10-18 16:11:21 +0900

    [UPDATE] ErrorCode 추가

--- src/main/java/com/alchemist/bianca/error/exception/ErrorCode.java
+++ src/main/java/com/alchemist/bianca/error/exception/ErrorCode.java
@@ -13,7 +13,11 @@ public enum ErrorCode {
     INVALID_TOKEN(401, "Invalid token"),
     EXPIRED_ACCESS_TOKEN(401, "Expired access token"),
     EXPIRED_REFRESH_TOKEN(401, "Expired refresh token"),
-    MISMATCHED_PASSWORD(400, "Mismatched password");
+    MISMATCHED_PASSWORD(400, "Mismatched password"),
+    ALREADY_EXIST_EMAIL(400, "Already Exist Email"),
+    ALREADY_EXIST_NAME(400, "Already Exist Name"),
+    INVALID_CODE(400, "Invalid code"),
+    UNLIKE_CODE(400, "Unlike code");
 
     private final int status;
     private final String message;
