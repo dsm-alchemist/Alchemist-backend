736fad8cd1f7e9a6e68de3e8e003e5d69b98a147
parksangwoo <b01056721617@gmail.com>
2021-10-18 16:06:33 +0900

    [UPDATE] notnull import 변경, email 어노테이션 추가

--- src/main/java/com/alchemist/bianca/dto/user/request/LoginRequest.java
+++ src/main/java/com/alchemist/bianca/dto/user/request/LoginRequest.java
@@ -1,15 +1,19 @@
 package com.alchemist.bianca.dto.user.request;
 
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
 public class LoginRequest {
 
-    @NotNull
+    @NotNull(message = "email은 null일 수 없습니다.")
+    @Email
     private String email;
-    @NotNull
+
+    @NotNull(message = "password는 null일 수 없습니다.")
     private String password;
 }
