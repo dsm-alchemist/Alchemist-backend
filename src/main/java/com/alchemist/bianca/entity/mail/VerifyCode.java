2765ede17da9f371aeebfa629710f126e4146d15
parksangwoo <b01056721617@gmail.com>
2021-10-18 16:09:33 +0900

    [ADD] VerifyCode Entity

---
+++ src/main/java/com/alchemist/bianca/entity/mail/VerifyCode.java
@@ -0,0 +1,30 @@
+package com.alchemist.bianca.entity.mail;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Id;
+
+@Getter
+@RedisHash
+@AllArgsConstructor
+public class VerifyCode {
+
+    @Id
+    private final String address;
+
+    @Indexed
+    private String code;
+
+    @TimeToLive
+    private Long ttl;
+
+    public VerifyCode update(String code, Long ttl) {
+        this.code = code;
+        this.ttl = ttl;
+        return this;
+    }
+}
