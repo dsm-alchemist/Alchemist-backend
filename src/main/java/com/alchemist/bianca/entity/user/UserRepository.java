ca784d0fe6767e5600496cd26a565a25d08028b6
parksangwoo <b01056721617@gmail.com>
2021-10-18 16:11:01 +0900

    [UPDATE] findByName 메소드 추가

--- src/main/java/com/alchemist/bianca/entity/user/UserRepository.java
+++ src/main/java/com/alchemist/bianca/entity/user/UserRepository.java
@@ -1,6 +1,11 @@
 package com.alchemist.bianca.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, String>, UserRepositoryCustom {
+    Optional<User> findByName(String name);
 }
