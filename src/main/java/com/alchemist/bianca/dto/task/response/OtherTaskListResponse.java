c4e9f008cc2f0c375e4383536197b8e799561dba
parksangwoo <b01056721617@gmail.com>
2021-10-19 19:05:51 +0900

    [ADD] OtherTaskListResponse

---
+++ src/main/java/com/alchemist/bianca/dto/task/response/OtherTaskListResponse.java
@@ -0,0 +1,15 @@
+package com.alchemist.bianca.dto.task.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class OtherTaskListResponse {
+    private final String userName;
+    private final List<TaskList> taskList;
+}
