19f2af977575d1510b3aab0d5150d022785331a4
parksangwoo <b01056721617@gmail.com>
2021-10-19 19:06:22 +0900

    [ADD] FollowCountResponse

---
+++ src/main/java/com/alchemist/bianca/dto/user/response/FollowCountResponse.java
@@ -0,0 +1,11 @@
+package com.alchemist.bianca.dto.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FollowCountResponse {
+    private final int following;
+    private final int follower;
+}
