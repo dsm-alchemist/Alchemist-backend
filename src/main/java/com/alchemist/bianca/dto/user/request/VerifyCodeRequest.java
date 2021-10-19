eb1e4ddaf40dce230dc62c6d929b8f2542c817bd
parksangwoo <b01056721617@gmail.com>
2021-10-19 10:24:02 +0900

    [UPDATE] email 필드 삭제

--- src/main/java/com/alchemist/bianca/dto/user/request/VerifyCodeRequest.java
+++ src/main/java/com/alchemist/bianca/dto/user/request/VerifyCodeRequest.java
@@ -3,16 +3,11 @@ package com.alchemist.bianca.dto.user.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
 public class VerifyCodeRequest {
-
-    @NotNull(message = "email은 null일 수 없습니다.")
-    @Email
-    private String email;
 
     @NotNull(message = "code는 null일 수 없습니다.")
     private String code;
