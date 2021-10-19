0e26673fa366919e6bda4a03eeda77ae34581b2c
parksangwoo <b01056721617@gmail.com>
2021-10-19 19:08:24 +0900

    [UPDATE] select 메소드 추가

--- src/main/java/com/alchemist/bianca/entity/task/TaskRepository.java
+++ src/main/java/com/alchemist/bianca/entity/task/TaskRepository.java
@@ -3,6 +3,10 @@ package com.alchemist.bianca.entity.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAllByDate(LocalDate date);
 }
