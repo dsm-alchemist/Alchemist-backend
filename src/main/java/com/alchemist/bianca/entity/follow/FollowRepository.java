a9d0cdbce40c68477c0c8101844a1c75630b7a5f
parksangwoo <b01056721617@gmail.com>
2021-10-19 19:07:12 +0900

    [UPDATE] select 메소드 추가

--- src/main/java/com/alchemist/bianca/entity/follow/FollowRepository.java
+++ src/main/java/com/alchemist/bianca/entity/follow/FollowRepository.java
@@ -1,8 +1,14 @@
 package com.alchemist.bianca.entity.follow;
 
+import com.alchemist.bianca.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, String> {
+    List<User> findAllByFollower(String email);
+    List<User> findAllByFollowing(String email);
 }
