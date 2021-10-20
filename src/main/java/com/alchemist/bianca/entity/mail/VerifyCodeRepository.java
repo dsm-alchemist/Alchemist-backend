4c726e28a1ed316a909d10c17d631eeabe4364a3
parksangwoo <b01056721617@gmail.com>
2021-10-20 19:44:27 +0900

    [UPDATE] extends 변경

--- src/main/java/com/alchemist/bianca/entity/mail/VerifyCodeRepository.java
+++ src/main/java/com/alchemist/bianca/entity/mail/VerifyCodeRepository.java
@@ -1,8 +1,9 @@
 package com.alchemist.bianca.entity.mail;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VerifyCodeRepository extends JpaRepository<VerifyCode, String> {
+public interface VerifyCodeRepository extends CrudRepository<VerifyCode, String> {
 }
