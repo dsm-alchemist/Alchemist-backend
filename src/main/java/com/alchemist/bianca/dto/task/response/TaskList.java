e152d072f5fc55bd45d3d6e6c1cb906acbb3ceb5
parksangwoo <b01056721617@gmail.com>
2021-10-20 19:15:30 +0900

    [UPDATE] QueryProjection 어노테이션 추가

--- src/main/java/com/alchemist/bianca/dto/task/response/TaskList.java
+++ src/main/java/com/alchemist/bianca/dto/task/response/TaskList.java
@@ -1,13 +1,20 @@
 package com.alchemist.bianca.dto.task.response;
 
-import lombok.AllArgsConstructor;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 @Builder
 public class TaskList {
+    private final Long task_id;
     private final String task;
     private final boolean isDone;
+
+    @QueryProjection
+    public TaskList(Long task_id,String task, boolean isDone) {
+        this.task_id = task_id;
+        this.task = task;
+        this.isDone = isDone;
+    }
 }
