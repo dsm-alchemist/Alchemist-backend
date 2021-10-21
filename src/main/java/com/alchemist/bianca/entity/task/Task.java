d1ba9dbbbc8535aa449a2c92d73fd72d14fbede7
parksangwoo <b01056721617@gmail.com>
2021-10-21 15:58:08 +0900

    [UPDATE] nullable 조건 추가

--- src/main/java/com/alchemist/bianca/entity/task/Task.java
+++ src/main/java/com/alchemist/bianca/entity/task/Task.java
@@ -1,8 +1,10 @@
 package com.alchemist.bianca.entity.task;
 
 import com.alchemist.bianca.entity.user.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -10,19 +12,22 @@ import java.time.LocalDate;
 @Entity
 @Getter
 @NoArgsConstructor
+@Builder
 public class Task {
     @Id
     @GeneratedValue
     private Long task_id;
 
-    @Column(length = 45)
+    @Column(length = 45, nullable = false)
     private String task;
 
+    @Column(nullable = false)
     private LocalDate date;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "email")
+    @JoinColumn(name = "email", nullable = false)
     private User email;
 
+    @ColumnDefault(value = "false")
     private boolean isDone;
 }
