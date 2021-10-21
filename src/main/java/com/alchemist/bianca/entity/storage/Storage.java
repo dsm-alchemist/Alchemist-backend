5593a3b9c75fc9b15b36cddb1d9e7faf953eff6f
parksangwoo <b01056721617@gmail.com>
2021-10-21 15:55:37 +0900

    [UPDATE] nullable 조건 추가

--- src/main/java/com/alchemist/bianca/entity/storage/Storage.java
+++ src/main/java/com/alchemist/bianca/entity/storage/Storage.java
@@ -1,6 +1,7 @@
 package com.alchemist.bianca.entity.storage;
 
 import com.alchemist.bianca.entity.user.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,15 +10,16 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor
+@Builder
 public class Storage {
     @Id
     @GeneratedValue
     private Long storage_id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "email")
+    @JoinColumn(name = "email", nullable = false)
     private User email;
 
-    @Column(length = 45)
+    @Column(length = 45, nullable = false)
     private String task;
 }
