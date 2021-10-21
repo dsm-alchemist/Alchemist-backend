47dd5d1507b7475518191633db4373c265d8bc55
parksangwoo <b01056721617@gmail.com>
2021-10-21 22:49:46 +0900

    [ADD] AuthController

---
+++ src/main/java/com/alchemist/bianca/controller/user/AuthController.java
@@ -0,0 +1,58 @@
+package com.alchemist.bianca.controller.user;
+
+import com.alchemist.bianca.dto.user.request.*;
+import com.alchemist.bianca.dto.user.response.TokenResponse;
+import com.alchemist.bianca.service.user.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signup(@RequestBody @Valid SignUpRequest request) {
+        authService.signUp(request);
+    }
+
+    @GetMapping("/reduplication/{email}")
+    @ResponseStatus(HttpStatus.OK)
+    public void overlapEmail(@PathVariable("email") @Valid EmailRequest request) {
+        authService.overlapEmail(request);
+    }
+
+    @GetMapping("/reduplication/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public void overlapName(@PathVariable("name") @Valid NameRequest request) {
+        authService.overlapName(request);
+    }
+
+    @PostMapping("/sms-certification/sends")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void sendCode(@RequestBody @Valid EmailRequest request) {
+        authService.sendCode(request);
+    }
+
+    @GetMapping("/sms-certification/confirms")
+    @ResponseStatus(HttpStatus.OK)
+    public void checkCode(@RequestParam("code") @Valid VerifyCodeRequest request) {
+        authService.checkCode(request);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest request) {
+        return authService.login(request);
+    }
+
+    @PostMapping("/refresh")
+    public TokenResponse tokenRefresh(@RequestBody @Valid RefreshTokenRequest request) {
+        return authService.tokenRefresh(request);
+    }
+}
