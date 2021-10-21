c3e8b69e7be68e72f29a6efa07bb94804c7b2142
parksangwoo <b01056721617@gmail.com>
2021-10-21 16:03:58 +0900

    [UPDATE] 메소드 추가

--- src/main/java/com/alchemist/bianca/service/task/TaskService.java
+++ src/main/java/com/alchemist/bianca/service/task/TaskService.java
@@ -1,19 +1,26 @@
 package com.alchemist.bianca.service.task;
 
+import com.alchemist.bianca.dto.task.request.AddTaskRequest;
 import com.alchemist.bianca.dto.task.request.TaskRequest;
-import com.alchemist.bianca.dto.task.response.OtherTaskListResponse;
+import com.alchemist.bianca.dto.task.response.TaskListResponse;
 import com.alchemist.bianca.dto.task.response.TaskList;
+import com.alchemist.bianca.entity.storage.Storage;
+import com.alchemist.bianca.entity.storage.StorageRepository;
 import com.alchemist.bianca.entity.task.Task;
 import com.alchemist.bianca.entity.task.TaskRepository;
+import com.alchemist.bianca.entity.user.User;
 import com.alchemist.bianca.entity.user.UserRepository;
+import com.alchemist.bianca.exception.TaskNotFoundException;
 import com.alchemist.bianca.exception.UserNotFoundException;
+import com.alchemist.bianca.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Comparator;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,26 +28,78 @@ public class TaskService {
 
     private final UserRepository userRepository;
     private final TaskRepository taskRepository;
+    private final StorageRepository storageRepository;
+    private final UserFacade userFacade;
 
-    @Transactional
-    public OtherTaskListResponse getOtherTaskList(TaskRequest request) {
-        userRepository.findById(request.getEmail())
-                .map(user -> OtherTaskListResponse.builder()
-                        .userName(user.getName())
-                        .build())
+    public void addTask(AddTaskRequest request) {
+        User user = userRepository.findByName(userFacade.getEmail())
                 .orElseThrow(UserNotFoundException::new);
 
-        List<TaskList> taskList = taskRepository.findAllByDate(request.getDate())
-                .stream()
-                .sorted(Comparator.comparing(Task::getTask_id))
-                .map(tasks -> TaskList.builder()
-                        .task(tasks.getTask())
-                        .isDone(tasks.isDone())
-                        .build())
-                .collect(Collectors.toList());
+        taskRepository.save(
+                Task.builder()
+                        .task(request.getTask())
+                        .date(request.getDate())
+                        .email(user)
+                        .build()
+        );
+    }
 
-        return OtherTaskListResponse.builder()
-                .taskList(taskList)
-                .build();
+    @Transactional
+    public ResponseEntity<TaskListResponse> getOtherTaskList(TaskRequest request) { //파라미터 변경
+        String name = getName(request.getEmail());
+
+        List<TaskList> taskList = taskRepository.getTaskList(request);
+
+        return new ResponseEntity<>(
+                TaskListResponse.builder()
+                        .userName(name)
+                        .taskList(taskList)
+                        .build(), HttpStatus.OK);
+    }
+
+    @Transactional
+    public ResponseEntity<TaskListResponse> getMyTaskList(TaskRequest request) {
+        String name = getName(userFacade.getEmail());
+
+        List<TaskList> taskList = taskRepository.getTaskList(request);
+
+        return new ResponseEntity<>(
+                TaskListResponse.builder()
+                        .userName(name)
+                        .taskList(taskList)
+                        .build(), HttpStatus.OK);
+    }
+
+    public void modifyTask(Long task_id, String task) {
+        taskRepository.modifyTask(task_id, task);
+    }
+
+    public void modifyDate(Long task_id, LocalDate date) {
+        taskRepository.modifyDate(task_id, date);
+    }
+
+    public void deleteTask(Long task_id) {
+        taskRepository.deleteTask(task_id);
+    }
+
+    @Transactional
+    public void moveTaskToStorage(Long task_id) {
+        Task task = taskRepository.findById(task_id)
+                .orElseThrow(TaskNotFoundException::new);
+
+        storageRepository.save(
+                Storage.builder()
+                        .task(task.getTask())
+                        .email(task.getEmail())
+                        .build()
+        );
+
+        taskRepository.delete(task);
+    }
+
+    private String getName(String email) {
+        return userRepository.findById(email)
+                .orElseThrow(UserNotFoundException::new)
+                .getClass().getName();
     }
 }
