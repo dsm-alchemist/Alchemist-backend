83ab74fcc768c37e89450c3d64fbc05fda5cf42c
parksangwoo <b01056721617@gmail.com>
2021-10-21 15:58:50 +0900

    [UPDATE] 추가 메소드 구현

--- src/main/java/com/alchemist/bianca/entity/task/TaskRepositoryCustomImpl.java
+++ src/main/java/com/alchemist/bianca/entity/task/TaskRepositoryCustomImpl.java
@@ -3,20 +3,23 @@ package com.alchemist.bianca.entity.task;
 import com.alchemist.bianca.dto.task.request.TaskRequest;
 import com.alchemist.bianca.dto.task.response.QTaskList;
 import com.alchemist.bianca.dto.task.response.TaskList;
+import com.alchemist.bianca.facade.UserFacade;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static com.alchemist.bianca.entity.task.QTask.task1;
-import static com.alchemist.bianca.entity.user.QUser.user;
+import static com.alchemist.bianca.entity.user.QUser.*;
 import static org.springframework.util.StringUtils.hasText;
 
 @RequiredArgsConstructor
 public class TaskRepositoryCustomImpl implements TaskRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
+    private final UserFacade userFacade;
 
     @Override
     public List<TaskList> getTaskList(TaskRequest request) {
@@ -35,7 +38,33 @@ public class TaskRepositoryCustomImpl implements TaskRepositoryCustom {
                 .fetch();
     }
 
+    @Override
+    public void modifyTask(Long task_id, String task) {
+        queryFactory
+                .update(task1)
+                .set(task1.task, task)
+                .where(task1.task_id.eq(task_id))
+                .execute();
+    }
+
+    @Override
+    public void modifyDate(Long task_id, LocalDate date) {
+        queryFactory
+                .update(task1)
+                .set(task1.date, date)
+                .where(task1.task_id.eq(task_id))
+                .execute();
+    }
+
+    @Override
+    public void deleteTask(Long task_id) {
+        queryFactory
+                .delete(task1)
+                .where(task1.task_id.eq(task_id))
+                .execute();
+    }
+
     private BooleanExpression emailEq(String email) {
-        return hasText(email) ? user.email.eq(email) : null;
+        return hasText(email) ? user.email.eq(email) : user.email.eq(userFacade.getEmail());
     }
 }
