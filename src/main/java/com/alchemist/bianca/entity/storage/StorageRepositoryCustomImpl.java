055f6293472ecf2e4e498d71792f6aad8867bd62
parksangwoo <b01056721617@gmail.com>
2021-10-21 15:56:30 +0900

    [ADD] StorageRepositoryCustomImpl

---
+++ src/main/java/com/alchemist/bianca/entity/storage/StorageRepositoryCustomImpl.java
@@ -0,0 +1,48 @@
+package com.alchemist.bianca.entity.storage;
+
+import com.alchemist.bianca.dto.storage.response.QStorageList;
+import com.alchemist.bianca.dto.storage.response.StorageList;
+import com.alchemist.bianca.entity.user.QUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.alchemist.bianca.entity.storage.QStorage.*;
+import static com.alchemist.bianca.entity.user.QUser.*;
+
+@RequiredArgsConstructor
+public class StorageRepositoryCustomImpl implements StorageRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<StorageList> getStorageList(String email) {
+        return queryFactory
+                .select(new QStorageList(
+                        storage.storage_id,
+                        storage.task
+                ))
+                .from(storage)
+                .join(storage.email, user)
+                .where(user.email.eq(email))
+                .fetch();
+    }
+
+    @Override
+    public void modifyStorage(Long storage_id, String task) {
+        queryFactory
+                .update(storage)
+                .set(storage.task, task)
+                .where(storage.storage_id.eq(storage_id))
+                .execute();
+    }
+
+    @Override
+    public void deleteStorage(Long storage_id) {
+        queryFactory
+                .delete(storage)
+                .where(storage.storage_id.eq(storage_id))
+                .execute();
+    }
+}
