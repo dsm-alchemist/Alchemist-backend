0f7429356843c5bb227f9446f41fab8d6f9559f6
parksangwoo <b01056721617@gmail.com>
2021-10-21 15:56:00 +0900

    [UPDATE] extends 추가

--- src/main/java/com/alchemist/bianca/entity/storage/StorageRepository.java
+++ src/main/java/com/alchemist/bianca/entity/storage/StorageRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StorageRepository extends JpaRepository<Storage, Long> {
+public interface StorageRepository extends JpaRepository<Storage, Long>, StorageRepositoryCustom {
 }
