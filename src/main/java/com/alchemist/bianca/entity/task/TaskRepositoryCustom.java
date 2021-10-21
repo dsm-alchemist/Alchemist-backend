818b8dcb8538890c97389926530ec9ec75457cf2
parksangwoo <b01056721617@gmail.com>
2021-10-21 15:58:33 +0900

    [UPDATE] 메소드 추가

--- src/main/java/com/alchemist/bianca/entity/task/TaskRepositoryCustom.java
+++ src/main/java/com/alchemist/bianca/entity/task/TaskRepositoryCustom.java
@@ -3,8 +3,12 @@ package com.alchemist.bianca.entity.task;
 import com.alchemist.bianca.dto.task.request.TaskRequest;
 import com.alchemist.bianca.dto.task.response.TaskList;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TaskRepositoryCustom {
     List<TaskList> getTaskList(TaskRequest request);
+    void modifyTask(Long task_id, String task);
+    void modifyDate(Long task_id, LocalDate date);
+    void deleteTask(Long task_id);
 }
