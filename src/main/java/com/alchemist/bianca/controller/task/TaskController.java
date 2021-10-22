c7a21d779565e16d5d1fc8406a501d196149ee76
parksangwoo <b01056721617@gmail.com>
2021-10-22 22:58:21 +0900

    [ADD] TaskController

---
+++ src/main/java/com/alchemist/bianca/controller/task/TaskController.java
@@ -0,0 +1,54 @@
+package com.alchemist.bianca.controller.task;
+
+import com.alchemist.bianca.dto.task.request.AddTaskRequest;
+import com.alchemist.bianca.dto.task.response.TaskListResponse;
+import com.alchemist.bianca.service.task.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/task")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @GetMapping
+    public ResponseEntity<TaskListResponse> getTaskList(@RequestParam LocalDate date) {
+        return taskService.getMyTaskList(date);
+    }
+
+    @PutMapping("/{taskId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void modifyTask(@PathVariable("taskId") Long task_id, @RequestBody String task) {
+        taskService.modifyTask(task_id, task);
+    }
+
+    @PostMapping("/{taskId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void modifyDate(@PathVariable("taskId") Long task_id, @RequestParam LocalDate date) {
+        taskService.modifyDate(task_id, date);
+    }
+
+    @DeleteMapping("/{taskId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable("taskId") Long task_id) {
+        taskService.deleteTask(task_id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addTask(@RequestBody AddTaskRequest request) {
+        taskService.addTask(request);
+    }
+
+    @PostMapping("/{taskId}/storage")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void moveTaskToStorage(@PathVariable("taskId") Long task_id) {
+        taskService.moveTaskToStorage(task_id);
+    }
+}
