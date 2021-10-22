08920f014ddc8387ddfc4c69cb24099a6750e017
parksangwoo <b01056721617@gmail.com>
2021-10-22 22:58:12 +0900

    [ADD] StorageController

---
+++ src/main/java/com/alchemist/bianca/controller/task/StorageController.java
@@ -0,0 +1,49 @@
+package com.alchemist.bianca.controller.task;
+
+import com.alchemist.bianca.dto.storage.request.AddStorageRequest;
+import com.alchemist.bianca.dto.storage.response.StorageList;
+import com.alchemist.bianca.service.task.StorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StorageController {
+
+    private final StorageService storageService;
+
+    @PostMapping("/task/storage")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addStorage(@RequestBody @Valid AddStorageRequest request) {
+        storageService.addStorage(request);
+    }
+
+    @PutMapping("/task/storage/{storageId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void modifyStorage(@PathVariable("storageId") Long storage_id, @RequestBody String task) {
+        storageService.modifyStorage(storage_id, task);
+    }
+
+    @PostMapping("/task/storage/{storageId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void moveTaskToStorage(@PathVariable("storageId") Long storage_id, @RequestParam LocalDate date) {
+        storageService.moveStorageToTask(storage_id, date);
+    }
+
+    @DeleteMapping("/task/storage/{storageId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteStorage(@PathVariable("storageId") Long storage_id) {
+        storageService.deleteStorage(storage_id);
+    }
+
+    @GetMapping("/task/storage")
+    public ResponseEntity<List<StorageList>> getStorageList() {
+        return storageService.getStorageList();
+    }
+}
