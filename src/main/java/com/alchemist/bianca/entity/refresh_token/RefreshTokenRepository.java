33276c782c759ea15dd4ad29e60d4a1090fb4d4c
parksangwoo <b01056721617@gmail.com>
2021-10-25 02:59:28 +0900

    [UPDATE] 불필요한 import 제거

--- src/main/java/com/alchemist/bianca/entity/refresh_token/RefreshTokenRepository.java
+++ src/main/java/com/alchemist/bianca/entity/refresh_token/RefreshTokenRepository.java
@@ -1,6 +1,5 @@
 package com.alchemist.bianca.entity.refresh_token;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
