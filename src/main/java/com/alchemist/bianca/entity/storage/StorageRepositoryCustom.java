79f1fc0f260122c0ce4998648efe2f310ce8395b
parksangwoo <b01056721617@gmail.com>
2021-10-25 23:24:59 +0900

    [UPDATE] 파라미터 변경

--- src/main/java/com/alchemist/bianca/entity/storage/StorageRepositoryCustom.java
+++ src/main/java/com/alchemist/bianca/entity/storage/StorageRepositoryCustom.java
@@ -1,11 +1,12 @@
 package com.alchemist.bianca.entity.storage;
 
+import com.alchemist.bianca.dto.storage.request.StorageRequest;
 import com.alchemist.bianca.dto.storage.response.StorageList;
 
 import java.util.List;
 
 public interface StorageRepositoryCustom {
     List<StorageList> getStorageList(String email);
-    void modifyStorage(Long storage_id, String task);
+    void modifyStorage(Long storage_id, StorageRequest task);
     void deleteStorage(Long storage_id);
 }
