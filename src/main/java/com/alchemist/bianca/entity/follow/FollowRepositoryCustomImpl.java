1909ece74484a789c8621a8a2e44ff3b215aead0
parksangwoo <b01056721617@gmail.com>
2021-10-25 23:24:47 +0900

    [ADD] FollowRepositoryCustomImpl

---
+++ src/main/java/com/alchemist/bianca/entity/follow/FollowRepositoryCustomImpl.java
@@ -0,0 +1,46 @@
+package com.alchemist.bianca.entity.follow;
+
+import com.alchemist.bianca.dto.user.response.UserListResponse;
+import com.alchemist.bianca.entity.user.User;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.alchemist.bianca.entity.follow.QFollow.*;
+import static com.alchemist.bianca.entity.user.QUser.*;
+
+@RequiredArgsConstructor
+public class FollowRepositoryCustomImpl implements FollowRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Follow> getFollowerList(String email) {
+        return queryFactory
+                .selectFrom(follow)
+                .join(follow.following, user)
+                .where(user.email.eq(email))
+                .fetch();
+    }
+
+    @Override
+    public List<Follow> getFollowingList(String email) {
+        return queryFactory
+                .selectFrom(follow)
+                .join(follow.follower, user)
+                .where(user.email.eq(email))
+                .fetch();
+    }
+
+    @Override
+    public void deleteFollow(User follower, User following) {
+        queryFactory
+                .delete(follow)
+                .where(
+                        follow.follower.eq(follower),
+                        follow.following.eq(following)
+                )
+                .execute();
+    }
+}
