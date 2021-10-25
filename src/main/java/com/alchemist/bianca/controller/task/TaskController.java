5ed504d79f415d3e372acd7885d987146341fe1d
parksangwoo <b01056721617@gmail.com>
2021-10-25 23:20:46 +0900

    [UPDATE] requestparam date type 설정

--- src/main/java/com/alchemist/bianca/controller/task/TaskController.java
+++ src/main/java/com/alchemist/bianca/controller/task/TaskController.java
@@ -1,9 +1,11 @@
 package com.alchemist.bianca.controller.task;
 
 import com.alchemist.bianca.dto.task.request.AddTaskRequest;
+import com.alchemist.bianca.dto.task.request.TaskRequest;
 import com.alchemist.bianca.dto.task.response.TaskListResponse;
 import com.alchemist.bianca.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +20,19 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping
-    public ResponseEntity<TaskListResponse> getTaskList(@RequestParam LocalDate date) {
+    public ResponseEntity<TaskListResponse> getTaskList(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return taskService.getMyTaskList(date);
     }
 
     @PutMapping("/{taskId}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void modifyTask(@PathVariable("taskId") Long task_id, @RequestBody String task) {
+    public void modifyTask(@PathVariable("taskId") Long task_id, @RequestBody TaskRequest task) {
         taskService.modifyTask(task_id, task);
     }
 
     @PostMapping("/{taskId}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void modifyDate(@PathVariable("taskId") Long task_id, @RequestParam LocalDate date) {
+    public void modifyDate(@PathVariable("taskId") Long task_id, @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         taskService.modifyDate(task_id, date);
     }
 
