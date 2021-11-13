a78fdbfbfa3f64e6d248a7366ba9415bad2343c3
parksangwoo <b01056721617@gmail.com>
2021-11-13 21:51:12 +0900

    [ADD] LankResponseDTO

---
+++ src/main/java/com/alchemist/bianca/dto/user/response/LankResponse.java
@@ -0,0 +1,20 @@
+package com.alchemist.bianca.dto.user.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LankResponse {
+    private final String name;
+    private final Long timer;
+    private final Boolean is_stop;
+
+    @QueryProjection
+    public LankResponse(String name, Long timer, Boolean is_stop) {
+        this.name = name;
+        this.timer = timer;
+        this.is_stop = is_stop;
+    }
+}
