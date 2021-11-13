f44f5963c05a15199b4eaa8b1e1b26a53c9c4b67
parksangwoo <b01056721617@gmail.com>
2021-11-13 23:07:43 +0900

    [ADD] Scheduler Configuration

---
+++ src/main/java/com/alchemist/bianca/config/Scheduler.java
@@ -0,0 +1,20 @@
+package com.alchemist.bianca.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class BatchConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public F
+}
