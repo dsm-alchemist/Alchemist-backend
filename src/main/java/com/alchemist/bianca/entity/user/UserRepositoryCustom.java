f29a2c8418fbe0383865cd2c9c9e9495108fd005
parksangwoo <b01056721617@gmail.com>
2021-11-13 21:51:51 +0900

    [ADD] UserRepositoryCustom

---
+++ src/main/java/com/alchemist/bianca/entity/user/UserRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.alchemist.bianca.entity.user;
+
+import com.alchemist.bianca.dto.user.response.LankResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface UserRepositoryCustom {
+    void startTimer(Long time);
+    void stopTimer();
+    Page<LankResponse> lank(Pageable pageable);
+}
