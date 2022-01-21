e9320cb179a023cb7fac60853e2ff32f58840c05
parksangwoo <b01056721617@gmail.com>
2022-01-21 20:04:06 +0900

    [ADD] ImageController

---
+++ src/main/java/com/alchemist/bianca/controller/image/ImageController.java
@@ -0,0 +1,23 @@
+package com.alchemist.bianca.controller.image;
+
+import com.alchemist.bianca.dto.image.response.SaveImageResponse;
+import com.alchemist.bianca.service.image.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageController {
+    private final ImageService imageService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    SaveImageResponse attachment(@RequestParam("file") MultipartFile file) {
+        return imageService.saveAttachment(file);
+    }
+}
