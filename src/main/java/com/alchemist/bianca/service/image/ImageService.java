dedf65a049488accb71f8bb927f48ce7eb7e6ade
parksangwoo <b01056721617@gmail.com>
2022-01-24 18:10:30 +0900

    [UPDATE]

--- src/main/java/com/alchemist/bianca/service/image/ImageService.java
+++ src/main/java/com/alchemist/bianca/service/image/ImageService.java
@@ -2,7 +2,11 @@ package com.alchemist.bianca.service.image;
 
 import com.alchemist.bianca.dto.image.response.SaveImageResponse;
 import com.alchemist.bianca.entity.image.Image;
+import com.alchemist.bianca.entity.user.User;
+import com.alchemist.bianca.entity.user.UserRepository;
+import com.alchemist.bianca.exception.UserNotFoundException;
 import com.alchemist.bianca.facade.ImageFacade;
+import com.alchemist.bianca.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,9 +16,16 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageService {
 
     private final ImageFacade attachmentFacade;
+    private final UserFacade userFacade;
+    private final UserRepository userRepository;
 
     public SaveImageResponse saveAttachment(MultipartFile file) {
         Image image = attachmentFacade.saveAttachment(file);
+
+        User user = userRepository.findById(userFacade.getEmail())
+                .orElseThrow(UserNotFoundException::new);
+
+        user.setImage(image);
 
         return SaveImageResponse.builder()
                 .id(image.getId())
